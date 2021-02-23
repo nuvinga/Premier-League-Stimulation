905840daadd5b801da674e46c7eb2ac509019702
Nuvin Godakanda Arachchi <nu.vv.in@gmail.com>
2021-02-23 10:50:23 +0530

    Delete app directory

--- app/utilities/customErrors/NameMatchException.java
+++
@@ -1,9 +0,0 @@
-package utilities.customErrors;
-
-public class NameMatchException extends Exception{
-    @Override
-    public void printStackTrace(){
-        System.out.println("\nOOPS THAT'S AN ERROR- Club name already in use\nPlease consider to rename the club or check if your " +
-                "manager has already registered the club\n");
-    }
-}
